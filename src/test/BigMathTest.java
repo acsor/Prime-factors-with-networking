83f1399468d8736137710b158e9b68e29b2a70a7
n0ne <new.new.none@gmail.com>
2017-03-13 18:36:30 +0100

    Complete Problem 3.

---
+++ src/test/BigMathTest.java
@@ -0,0 +1,60 @@
+import org.junit.Assert;
+import org.junit.Test;
+import util.BigMath;
+
+import java.math.BigInteger;
+import java.util.List;
+
+/**
+ * Created by n0ne on 13/03/17.
+ */
+public class BigMathTest {
+
+	@Test
+	public void testPrimeFactorsOf () {
+		final BigInteger[] products = {
+				new BigInteger("85"),
+				new BigInteger("264"),
+				new BigInteger("1331"),
+				new BigInteger("18161"),
+				new BigInteger("26247"),
+				new BigInteger("256159"),
+				new BigInteger("658073"),
+		};
+		final BigInteger[][] expectedResults = {
+				{new BigInteger("5"), new BigInteger("17")},
+				{new BigInteger("2"), new BigInteger("2"), new BigInteger("2"), new BigInteger("3"), new BigInteger("11")},
+				{new BigInteger("11"), new BigInteger("11"), new BigInteger("11")},
+				{new BigInteger("11"), new BigInteger("13"), new BigInteger("127")},
+				{new BigInteger("3"), new BigInteger("13"), new BigInteger("673")},
+				{new BigInteger("127"), new BigInteger("2017")},
+				{new BigInteger("13"), new BigInteger("223"), new BigInteger("227")},
+		};
+		final int min = Math.min(products.length, expectedResults.length);
+		List<BigInteger> actualResult;
+
+		for (int i = 0; i < min; i++) {
+			actualResult = BigMath.primeFactorsOf(
+					products[i],
+					new BigInteger("2"),
+					products[i].divide(new BigInteger("2"))
+			);
+
+			Assert.assertEquals(
+					expectedResults[i],
+					actualResult.toArray(new BigInteger[expectedResults[i].length])
+			);
+		}
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testPrimeFactorsOfLessThanTwo () {
+		BigMath.primeFactorsOf(new BigInteger("1"), new BigInteger("4"), new BigInteger("10"));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testPrimeFactorsOfLowHighInvalid () {
+		BigMath.primeFactorsOf(new BigInteger("4040"), new BigInteger("11"), new BigInteger("1"));
+	}
+
+}
--- src/util/BigMath.java
+++ src/util/BigMath.java
@@ -1,31 +1,61 @@
 package util;
 
 import java.math.BigInteger;
+import java.util.LinkedList;
+import java.util.List;
 
 public class BigMath {
 
-    /**
-     * Given a BigInteger input n, where n >= 0, returns the largest BigInteger r such that r*r <= n.
-     * 
-     * For n < 0, returns 0.
-     * 
-     * 
-     * details: http://faruk.akgul.org/blog/javas-missing-algorithm-biginteger-sqrt
-     * 
-     * @param n BigInteger input.
-     * @return for n >= 0: largest BigInteger r such that r*r <= n.
-     *             n <  0: BigInteger 0
-     */
-    public static BigInteger sqrt(BigInteger n) {
-        BigInteger a = BigInteger.ONE;
-        BigInteger b = new BigInteger(n.shiftRight(5).add(new BigInteger("8")).toString());
-        while(b.compareTo(a) >= 0) {
-          BigInteger mid = new BigInteger(a.add(b).shiftRight(1).toString());
-          if (mid.multiply(mid).compareTo(n) > 0) 
-              b = mid.subtract(BigInteger.ONE);
-          else 
-              a = mid.add(BigInteger.ONE);
-        }
-        return a.subtract(BigInteger.ONE);
-    }
+	/**
+	 * Given a BigInteger input n, where n >= 0, returns the largest BigInteger r such that r*r <= n.
+	 * <p>
+	 * For n < 0, returns 0.
+	 * <p>
+	 * <p>
+	 * details: http://faruk.akgul.org/blog/javas-missing-algorithm-biginteger-sqrt
+	 *
+	 * @param n BigInteger input.
+	 * @return for n >= 0: largest BigInteger r such that r*r <= n.
+	 * n <  0: BigInteger 0
+	 */
+	public static BigInteger sqrt (BigInteger n) {
+		BigInteger a = BigInteger.ONE;
+		BigInteger b = new BigInteger(n.shiftRight(5).add(new BigInteger("8")).toString());
+		while (b.compareTo(a) >= 0) {
+			BigInteger mid = new BigInteger(a.add(b).shiftRight(1).toString());
+			if (mid.multiply(mid).compareTo(n) > 0)
+				b = mid.subtract(BigInteger.ONE);
+			else
+				a = mid.add(BigInteger.ONE);
+		}
+		return a.subtract(BigInteger.ONE);
+	}
+
+	public static List<BigInteger> primeFactorsOf (BigInteger n, BigInteger low, BigInteger high) {
+		final List<BigInteger> factors = new LinkedList<>();
+
+		if (n.compareTo(new BigInteger("2")) <= -1) {
+			throw new IllegalArgumentException(String.format("Parameter n has value %d < 2", n));
+		}
+		if (low.compareTo(BigInteger.ONE) <= -1 || high.compareTo(low) <= -1) {
+			throw new IllegalArgumentException(
+					String.format(
+							"low and high parameters are not such that 1 <= low (%s) <= high (%s)",
+							low, high
+					)
+			);
+		}
+
+		for (BigInteger divisor = low; divisor.compareTo(high) < 1; divisor = divisor.add(BigInteger.ONE)) {
+			if (divisor.isProbablePrime(100)) {
+				while (n.remainder(divisor).compareTo(BigInteger.ZERO) == 0) {
+					n = n.divide(divisor);
+					factors.add(divisor);
+				}
+			}
+		}
+
+		return factors;
+	}
+
 }
